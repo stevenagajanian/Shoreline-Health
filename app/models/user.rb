3193cad5c1f7523fcdb8c1432946c3c36d006f60
James Steininger <jesteininger@gmail.com>
2014-05-12 17:46:31 -0700

    Fixing date of birth

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,7 @@ accepts_nested_attributes_for :friends
 
 	validates :first_name, presence: true
 	validates :last_name, presence: true
+	validates :date_of_birth, presence: true
 	validates :email, presence: true
 
 	def full_name
