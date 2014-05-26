8dae4ad394682984c9d5f91e622a91946155b166
James Steininger <jesteininger@gmail.com>
2014-05-26 11:34:46 -0700

    Branding change to Komodo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515055717) do
+ActiveRecord::Schema.define(version: 20140522091845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,14 @@ ActiveRecord::Schema.define(version: 20140515055717) do
   end
 
   add_index "episodes", ["user_id"], name: "index_episodes_on_user_id", using: :btree
+
+  create_table "features", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "votes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "fitnesses", force: true do |t|
     t.integer  "user_id"
